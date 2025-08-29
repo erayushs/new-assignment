35179a14da2633d19d558b3bfad930089e186a83
Ayush Singh <anmol8960@gmail.com>
2025-08-29 13:23:04 +0530

    added icons

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,17 @@
+import locationIcon from "../../public/location.svg";
+import bellIcon from "../../public/bell.svg";
+import voucherIcon from "../../public/coupon.png";
+import walletIcon from "../../public/wallet.svg";
+import diamondIcon from "../../public/diamond.svg";
+import scannerIcon from "../../public/scanner.svg";
+import rightArrowIcon from "../../public/rightArrowIcon.png";
+import profileIcon from "../../public/profile.png";
+
 const Dashboard = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-100 overflow-hidden">
-      <div className="lg:w-[500px] w-full bg-white p-2 h-full relative">
-        <div className="w-full  h-[100px] fixed bottom-0 left-1/2 transform -translate-x-1/2 lg:w-[500px] bg-white">
+    <div className="flex w-screen items-center justify-center bg-gray-100 overflow-hidden">
+      <div className="lg:w-[500px] w-full bg-white p-2 relative">
+        <div className="w-full h-[100px] fixed bottom-0 left-1/2 transform -translate-x-1/2 lg:w-[500px] bg-white">
           <div className="flex p-4 justify-around">
             <div className="">
               <div className="w-[50px] h-[50px] border rounded-md"></div>
@@ -25,39 +34,80 @@ const Dashboard = () => {
         <div>
           <div className="mt-4 p-2 px-4 flex justify-between items-center">
             <div className="flex">
-              <div className="w-[50px] h-[50px] border rounded-full"></div>
+              <div className="w-[50px] h-[50px] rounded-full">
+                <img src={profileIcon} alt="profileIcon" />
+              </div>
               <div className="ml-4 justify-self-start mt-1">
-                <p className="text-sm text-gray-600">My Location</p>
-                <p className="text-md">
-                  <span>Ireland,</span> Dublin 24
+                <p className="text-sm text-gray-400">My Location</p>
+                <p className="text-md flex">
+                  <img
+                    src={locationIcon}
+                    alt="location"
+                    className="w-[15px] mr-1"
+                  />{" "}
+                  <span className="font-semibold">Ireland, </span> Dublin 24
                 </p>
               </div>
             </div>
 
-            <div className="w-[30px] h-[30px] border rounded-full"></div>
+            <img
+              src={bellIcon}
+              alt="bellIcon"
+              className="w-[30px] mb-2 cursor-pointer"
+            ></img>
           </div>
 
           <div className="border border-gray-300 w-[90%] rounded-2xl h-[120px] mx-auto mt-4">
             <div className="flex gap-2 justify-center p-2">
-              <div className="border-gray-300 border-r-2 w-[33%] h-[60px]">
-                <div className="border w-[40px] h-[40px] mx-auto mt-3 rounded-xl border-gray-400"></div>
+              <div className="border-gray-300 border-r-2 w-[24%] h-[60px]">
+                <div className=" w-[40px] h-[40px] mx-auto mt-3">
+                  <img src={scannerIcon} alt="scannerIcon" />
+                </div>
               </div>
-              <div className="border-gray-300 border-r-2 w-[33%] h-[60px] pt-1">
+              <div className="border-gray-300 border-r-2 w-[40%] h-[60px] pt-1">
                 <div className="flex flex-col justify-center ml-2 gap-0.5">
-                  <p className="text-sm text-gray-400">My Balance</p>
+                  <p className="text-sm text-gray-400 flex">
+                    <img
+                      src={walletIcon}
+                      alt="walletIcon"
+                      className="mr-1 w-[15px]"
+                    />{" "}
+                    My Balance
+                  </p>
                   <p className="text-xl">$121,98</p>
                 </div>
               </div>
-              <div className="border-gray-300 w-[33%] h-[60px]">
-                <div className="flex flex-col justify-center ml-2 gap-0.5 pt-1">
-                  <p className="text-sm text-gray-400">My Coins</p>
+              <div className="border-gray-300 w-[40%] h-[60px]">
+                <div className="flex flex-col justify-center ml-2 gap-0.5 pt-1 ">
+                  <p className="text-sm text-gray-400 flex">
+                    <img
+                      src={diamondIcon}
+                      alt="walletIcon"
+                      className="mr-1 w-[15px]"
+                    />{" "}
+                    My Coins
+                  </p>
                   <p className="text-xl">1,295</p>
                 </div>
               </div>
             </div>
             <div className="border-t border-gray-300"> </div>
 
-            <p className="p-2 pl-10">7 Vouchers Available</p>
+            <div className="flex justify-between items-center">
+              <p className="p-2 pl-6 flex">
+                <img
+                  src={voucherIcon}
+                  alt="voucher"
+                  className="w-[25px] mr-3"
+                />
+                7 Vouchers Available
+              </p>
+              <img
+                src={rightArrowIcon}
+                alt="right arrow"
+                className="w-[15px] mr-4 cursor-pointer"
+              />
+            </div>
           </div>
         </div>
 
@@ -101,9 +151,7 @@ const Dashboard = () => {
               </h3>
             </div>
 
-            <div className="mt-4 flex flex-col gap-4">
-              <div className="w-[95%] border h-[65px] mx-auto rounded-2xl"></div>
-              <div className="w-[95%] border h-[65px] mx-auto rounded-2xl"></div>
+            <div className="mt-4 flex flex-col gap-4 pb-20">
               <div className="w-[95%] border h-[65px] mx-auto rounded-2xl"></div>
               <div className="w-[95%] border h-[65px] mx-auto rounded-2xl"></div>
             </div>
