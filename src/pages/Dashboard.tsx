a4af3b9a2c7396c5f90d50b2b603f4c4d8a5ca2b
Ayush Singh <anmol8960@gmail.com>
2025-08-29 14:07:42 +0530

    complete UI with icons

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,6 +6,14 @@ import diamondIcon from "../../public/diamond.svg";
 import scannerIcon from "../../public/scanner.svg";
 import rightArrowIcon from "../../public/rightArrowIcon.png";
 import profileIcon from "../../public/profile.png";
+import thunderIcon from "../../public/thunder.png";
+import archiveIcon from "../../public/archive.png";
+import deliveryIcon from "../../public/delivery.png";
+import trackIcon from "../../public/map.png";
+import homeIcon from "../../public/home.svg";
+import activity from "../../public/activity.png";
+import message from "../../public/message.png";
+import account from "../../public/account.png";
 
 const Dashboard = () => {
   return (
@@ -14,19 +22,27 @@ const Dashboard = () => {
         <div className="w-full h-[100px] fixed bottom-0 left-1/2 transform -translate-x-1/2 lg:w-[500px] bg-white">
           <div className="flex p-4 justify-around">
             <div className="">
-              <div className="w-[50px] h-[50px] border rounded-md"></div>
+              <div className="w-[50px] h-[50px] rounded-md">
+                <img src={homeIcon} alt="" className="w-full p-2" />
+              </div>
               <p className="text-sm text-gray-600 text-center pt-2">Home</p>
             </div>
             <div className="">
-              <div className="w-[50px] h-[50px] border rounded-md"></div>
+              <div className="w-[50px] h-[50px] rounded-md p-[10px]">
+                <img src={activity} alt="" />
+              </div>
               <p className="text-sm text-gray-600 text-center pt-2">Activity</p>
             </div>
             <div className="">
-              <div className="w-[50px] h-[50px] border rounded-md"></div>
+              <div className="w-[50px] h-[50px] rounded-md p-[10px]">
+                <img src={message} alt="" />
+              </div>
               <p className="text-sm text-gray-600 text-center pt-2">Help</p>
             </div>
             <div className="">
-              <div className="w-[50px] h-[50px] border rounded-md"></div>
+              <div className="w-[50px] h-[50px] rounded-md p-[4px]">
+                <img src={account} alt="" />
+              </div>
               <p className="text-sm text-gray-600 text-center pt-2">Account</p>
             </div>
           </div>
@@ -115,19 +131,27 @@ const Dashboard = () => {
           <h3 className="text-lg font-bold">Our Service</h3>
           <div className="flex justify-between mt-4">
             <div className="flex flex-col items-center gap-2">
-              <div className="border border-amber-400 w-[70px] h-[70px] bg-amber-100 rounded-full"></div>
+              <div className="border border-amber-400 w-[70px] h-[70px] bg-amber-50 rounded-full">
+                <img src={thunderIcon} alt="thunderIcon" className="p-5" />
+              </div>
               <p className="text-sm text-gray-400">Express</p>
             </div>
             <div className="flex flex-col items-center gap-2">
-              <div className="border border-green-400 w-[70px] h-[70px] bg-green-100 rounded-full"></div>
+              <div className="border border-green-400 w-[70px] h-[70px] bg-green-100 rounded-full">
+                <img src={archiveIcon} alt="archiveIcon" className="p-4" />
+              </div>
               <p className="text-sm text-gray-400">Regular</p>
             </div>
             <div className="flex flex-col items-center gap-2">
-              <div className="border border-blue-400 w-[70px] h-[70px] bg-blue-100 rounded-full"></div>
+              <div className="border border-blue-400 w-[70px] h-[70px] bg-blue-100 rounded-full">
+                <img src={deliveryIcon} alt="deliveryIcon" className="p-4" />
+              </div>
               <p className="text-sm text-gray-400">Cargo</p>
             </div>
             <div className="flex flex-col items-center gap-2">
-              <div className="border border-amber-400 w-[70px] h-[70px] bg-amber-100 rounded-full"></div>
+              <div className="border border-amber-400 w-[70px] h-[70px] bg-amber-50 rounded-full">
+                <img src={trackIcon} alt="trackIcon" className="p-5" />
+              </div>
               <p className="text-sm text-gray-400">Tracking</p>
             </div>
           </div>
@@ -146,14 +170,56 @@ const Dashboard = () => {
           <div className="mt-8">
             <div className="flex justify-between">
               <h3 className="text-lg font-bold">My Package</h3>
-              <h3 className="text-md text-purple-400 font-semi-bold cursor-pointer">
+              <h3 className="text-md text-purple-400 font-semibold cursor-pointer">
                 See All
               </h3>
             </div>
 
             <div className="mt-4 flex flex-col gap-4 pb-20">
-              <div className="w-[95%] border h-[65px] mx-auto rounded-2xl"></div>
-              <div className="w-[95%] border h-[65px] mx-auto rounded-2xl"></div>
+              <div className="w-[95%] bg-gray-100 h-[65px] mx-auto rounded-2xl p-4">
+                <div className="flex justify-between">
+                  <div className="flex gap-4">
+                    <div className=" w-[35px] h-[35px]">
+                      <img
+                        src={thunderIcon}
+                        alt="thunderIcon"
+                        className="bg-white rounded p-1"
+                      />
+                    </div>
+                    <div className="text-sm">
+                      <h4 className="-mt-0.5">Express Service</h4>
+                      <p className="text-slate-400">USA, New York</p>
+                    </div>
+                  </div>
+
+                  <div className="text-[15px] text-right">
+                    <h1 className="-mt-1">est. Aug 25</h1>
+                    <p className="text-slate-400">Pyotr Kodzhebash</p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[95%] bg-gray-100 h-[65px] mx-auto rounded-2xl p-4">
+                <div className="flex justify-between">
+                  <div className="flex gap-4">
+                    <div className=" w-[35px] h-[35px] ">
+                      <img
+                        src={archiveIcon}
+                        alt="archiveIcon"
+                        className="bg-white rounded p-1"
+                      />
+                    </div>
+                    <div className="text-sm">
+                      <h4 className="-mt-0.5">Regular Service</h4>
+                      <p className="text-slate-400">USA, New York</p>
+                    </div>
+                  </div>
+
+                  <div className="text-[15px] text-right">
+                    <h1 className="-mt-1">est. Aug 25</h1>
+                    <p className="text-slate-400">Pyotr Kodzhebash</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
