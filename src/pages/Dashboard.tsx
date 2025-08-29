6308efdc83bcf74356fb8c44a56291cf23ffda80
Ayush Singh <anmol8960@gmail.com>
2025-08-29 10:24:02 +0530

    basic structure dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,25 @@
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashborad</h1>
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-100 overflow-hidden">
+      <div className="lg:w-[500px] w-full bg-white p-2 h-full relative">
+        <div className="border h-[300px]">
+          <div className="mt-4 p-2 px-4 flex justify-between items-center">
+            <div className="flex">
+              <div className="w-[50px] h-[50px] border rounded-full"></div>
+              <div className="ml-4 justify-self-start mt-1">
+                <p className="text-sm text-gray-600">My Location</p>
+                <p className="text-md">
+                  <span>Ireland,</span> Dublin 24
+                </p>
+              </div>
+            </div>
+
+            <div className="w-[30px] h-[30px] border rounded-full"></div>
+          </div>
+
+          <div className="border w-[90%] rounded-2xl h-[100px] mx-auto mt-4"></div>
+        </div>
+      </div>
     </div>
   );
 };
